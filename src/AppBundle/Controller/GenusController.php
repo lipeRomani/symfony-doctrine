d484cacd79110d886084ed64425194b0c4c8c7d8
Felipe Romani <felipe.romani@wideti.com.br>
2016-07-12 16:59:25 -0300

    primeiro commit

---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Genus;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenusController extends Controller
+{
+    /**
+     * @Route(path="/genus/new",name="genus_new")
+     */
+    public function newAction(){
+        $genus = new Genus();
+        $genus->setName("Octopus " . rand(1,100));
+        $genus->setSubFamily('Octopodinae');
+        $genus->setSpeciesCount(rand(100,9999));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($genus);
+        $em->flush();
+
+        return new Response("<html><body>Genus created!</body></html>");
+    }
+
+    /**
+     * @Route("/genus")
+     */
+    public function listAction(){
+        $em = $this->getDoctrine()->getManager();
+        $genus = $em->getRepository("AppBundle:Genus")->findAll();
+        return $this->render(':genus:list.html.twig',['genuses' => $genus]);
+    }
+
+    /**
+     * @Route("/genus/{genusName}",name="genus_show")
+     */
+    public function showAction($genusName)
+    {
+        /*$funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
+
+        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
+        $key = md5($funFact);
+        if ($cache->contains($key)) {
+            $funFact = $cache->fetch($key);
+        } else {
+            sleep(1); // fake how slow this could be
+            $funFact = $this->get('markdown.parser')
+                ->transform($funFact);
+            $cache->save($key, $funFact);
+        }
+
+        $this->get('logger')
+            ->info('Showing genus: '.$genusName);
+
+        return $this->render('genus/show.html.twig', array(
+            'name' => $genusName,
+            'funFact' => $funFact,
+        ));*/
+
+        $em = $this->getDoctrine()->getManager();
+        $genus = $em->getRepository('AppBundle:Genus')->findOneBy(['name' => $genusName]);
+
+        if(!$genus) throw $this->createNotFoundException();
+
+        return $this->render(':genus:show.html.twig',['genus' => $genus]);
+    }
+
+    /**
+     * @Route("/genus/{genusName}/notes", name="genus_show_notes")
+     * @Method("GET")
+     */
+    public function getNotesAction($genusName)
+    {
+        $notes = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+        ];
+        $data = [
+            'notes' => $notes
+        ];
+
+        return new JsonResponse($data);
+    }
+}
---
+++ src/AppBundle/Repository/GenusRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romani
+ * Date: 12/07/16
+ * Time: 15:03
+ */
+
+namespace AppBundle\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class GenusRepository extends EntityRepository
+{
+
+}
